5b3baa594bad1ab2954b59f2adb97a147ed3af88
mriduladas <mridula199@gmail.com>
2022-05-09 17:35:58 +0530

    Load all quotes from firebase

--- src/pages/QuoteDetails.js
+++ src/pages/QuoteDetails.js
@@ -2,25 +2,48 @@ import { Route } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { useRouteMatch } from "react-router-dom";
 import { useParams } from "react-router-dom";
+import { useEffect } from "react";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote"
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
 
-const allQuotes = [{id:'1',author:'Author A', text:'1st quote'}, {id:'2',author:'Author B', text:'2nd quote'}];
+
 
 const QuoteDetails =()=> {
+
+    const {sendRequest,status, data:loadedQuote, error} =useHttp(getSingleQuote,true);
+    const params =useParams();
+    useEffect(()=> {
+        sendRequest(params.id);
+    }, [sendRequest,params.id])
     const match =useRouteMatch();
     console.log(match);
-    const params =useParams();
+    
     //console.log('params');
-    console.log('filteredQuote');
-    const quote=allQuotes.filter((quote)=>quote.id===params.id)[0];
-    if(!quote){
+    //console.log('filteredQuote');
+    //const quote=allQuotes.filter((quote)=>quote.id===params.id)[0];
+    if(status=== 'pending'){
+        return(
+            <div className='centered'>
+                <LoadingSpinner></LoadingSpinner>
+            </div>
+        )
+    }
+    if(error){
+        return(
+            <p className='centered focused'>{error}</p>
+        )
+    }
+
+    if(!loadedQuote){
         return <p>No Quote Found</p>
     }
 
     return(
     <section>
-        <HighlightedQuote text={quote.text} author={quote.author}></HighlightedQuote>
+        <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author}></HighlightedQuote>
         <Route path={match.path} exact>
             <div className="centered">
                 <Link to={`${match.url}/comments`} className='btn--flat'>
