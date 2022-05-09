2e665f7334451090ceb5b10aedf9bc887eb4d63a
mriduladas <mridula199@gmail.com>
2022-05-09 16:50:27 +0530

    Used Http call for NewQuote.js

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,16 +1,28 @@
 import { useHistory } from "react-router-dom";
+import { useEffect } from "react";
 import QuoteForm from "../components/quotes/QuoteForm";
+import useHttp from "../hooks/use-http";
+import { addQuote } from "../lib/api";
 
 const NewQuote = () => {
+
+    const {sendRequest, status} = useHttp(addQuote);
+    
+    useEffect(()=>{
+        if(status=== 'completed'){
+            history.push('/quotes')
+        }
+    })
     
     const history = useHistory();
     const onAddQuoteHandler= (quote) => {
         console.log(quote);
-        history.push('/quotes')
+        sendRequest(quote);
+        
     }
 
     return(
-        <QuoteForm onAddQuote={onAddQuoteHandler}></QuoteForm>
+        <QuoteForm isLoading={status==='pending'}  onAddQuote={onAddQuoteHandler}></QuoteForm>
         )
 
 }
